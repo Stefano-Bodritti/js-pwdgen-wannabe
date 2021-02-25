8a618a5b2283e2573a3b61b458d4f83538587e22
Stefano <tepo@hotmail.it>
2021-02-25 15:44:33 +0100

    Generazione password

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,3 +1,6 @@
 var nome = prompt("Digita il tuo nome");
 var cognome = prompt("Digita il tuo cognome");
 var colore = prompt("Digita il tuo colore preferito");
+
+var pass = nome + cognome + colore + 27;
+document.getElementById('psw').innerHTML = pass;
